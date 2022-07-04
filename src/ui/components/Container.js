48ad494c7f88d0f86015fc17e59d05ab0fc58f6a
neglux <akcaaytuna@gmail.com>
2022-07-04 18:41:20 +0300

    fix: default prop value

--- src/ui/components/Container.js
+++ src/ui/components/Container.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Container = ({ children, style }) => {
+const Container = ({ children, style = "" }) => {
   return (
     <ul
       className={`flex flex-col justify-start items-start w-full h-0 mt-2 bg-gray-300 rounded-md shadow-md overflow-hidden ${style}`}
--- src/ui/components/Section.js
+++ src/ui/components/Section.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Section = ({ children, style }) => {
+const Section = ({ children, style = "" }) => {
   return <section className={`mx-10 my-5 ${style}`}>{children}</section>;
 };
 
