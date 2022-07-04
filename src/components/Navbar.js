5abdd2bb9bc8ea8ee87070f6c9c2602f9d439646
neglux <akcaaytuna@gmail.com>
2022-07-04 12:02:13 +0300

    develop: navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
+import strings from "../data/strings";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const { loginMsg, loginBtnText } = strings.navbar;
+  return (
+    <nav className="flex w-full items-center justify-between px-10 py-8 bg-gray-900 text-gray-100">
+      <span>{loginMsg}</span>
+      <button className="px-12 py-1 rounded-sm bg-gray-700 hover:bg-gray-600">
+        {loginBtnText}
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/data/strings.js
@@ -0,0 +1,6 @@
+export default {
+  navbar: {
+    loginMsg: "Login to get started",
+    loginBtnText: "Log In",
+  },
+};
