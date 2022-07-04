63b0f6266ee0a7e8216f27b30667c91888d8e936
neglux <akcaaytuna@gmail.com>
2022-07-04 12:03:10 +0300

    develop: menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,25 @@
 import React from "react";
+import strings from "../data/strings";
 
 const Menu = () => {
-  return <menu>Menu</menu>;
+  const { sections } = strings.menu;
+  const optItemStyle = "ml-auto";
+  return (
+    <menu className="flex w-full bg-gray-700 text-gray-100">
+      {sections.map((item, index) => {
+        return (
+          <li
+            key={index}
+            className={`px-12 py-2 text-sm hover:bg-gray-600 cursor-pointer border-x-[1px] border-gray-600 ${
+              index === sections.length - 1 ? optItemStyle : ""
+            }`}
+          >
+            {item}
+          </li>
+        );
+      })}
+    </menu>
+  );
 };
 
 export default Menu;
--- src/data/strings.js
+++ src/data/strings.js
@@ -3,4 +3,18 @@ export default {
     loginMsg: "Login to get started",
     loginBtnText: "Log In",
   },
+  menu: {
+    sections: ["Dashboard", "Transfer", "Loan", "Options"],
+  },
+  // dashboard: {
+  //   containerRate: {
+  //     title: "Currency Rates",
+  //   },
+  //   containerMovement: {
+  //     title: "Movements",
+  //   },
+  //   containerBalance: {
+  //     title: "Balance",
+  //   },
+  // },
 };
