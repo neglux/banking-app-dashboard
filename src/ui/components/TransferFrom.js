5447662c47d623ec1bf19bdb3a7b8906ba872216
neglux <akcaaytuna@gmail.com>
2022-07-05 09:15:07 +0300

    develop: transfer section

---
+++ src/ui/components/TransferFrom.js
@@ -0,0 +1,51 @@
+import React from "react";
+import strings from "../../data/strings";
+import Dropdown from "./Dropdown";
+import Input from "./Input";
+import TextBox from "./TextBox";
+import users from "../../data/users";
+import bank from "../../data/bank";
+
+const TransferFrom = () => {
+  const {
+    form: {
+      senderLabelText,
+      receiverLabelText,
+      amountLabelText,
+      currencyLabelText,
+      dateLabelText,
+      feeLabelText,
+      sendBtn,
+    },
+  } = strings.transfer;
+
+  const names = getNames(users);
+
+  function getNames(object) {
+    const array = object.map((item) => {
+      return `${item?.firstName} ${item?.lastName}`;
+    });
+
+    return array;
+  }
+
+  return (
+    <form className="ml-40 my-20">
+      <TextBox label={senderLabelText} text="John Doe" />
+      <Dropdown text={receiverLabelText} data={names} />
+      <div className="flex items-center">
+        <Input text={amountLabelText} />
+        <Dropdown text={currencyLabelText} data={bank.currencies} />
+      </div>
+      <div className="flex items-center">
+        <TextBox label={dateLabelText} text={"12/10/2028"} />
+        <TextBox label={feeLabelText} text={"$0.14"} />
+      </div>
+      <button className="mt-10 bg-green-500 text-gray-50 px-10 py-2 rounded hover:bg-green-600">
+        {sendBtn}
+      </button>
+    </form>
+  );
+};
+
+export default TransferFrom;
--- src/ui/modules/Transfer.js
+++ src/ui/modules/Transfer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Section from "../components/Section";
+import Container from "../components/Container";
+import TransferFrom from "../components/TransferFrom";
 
 const Transfer = () => {
-  return <section>Transfer</section>;
+  return (
+    <Section>
+      <Container style="min-h-[550px]">
+        <TransferFrom />
+      </Container>
+    </Section>
+  );
 };
 
 export default Transfer;
