05707718a73f6f2f455ccfd158624a253732bc53
neglux <akcaaytuna@gmail.com>
2022-07-06 13:38:02 +0300

    fix: overlay opacity

--- src/ui/components/Overlay.js
+++ src/ui/components/Overlay.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Overlay = () => {
-  return <div className="fixed w-screen h-screen bg-black opacity-75"></div>;
+  return <div className="fixed w-screen h-screen bg-black opacity-50"></div>;
 };
 
 export default Overlay;
