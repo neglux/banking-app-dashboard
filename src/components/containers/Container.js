725b4b09b6804d231a72a7318f787d55549fd6ff
neglux <akcaaytuna@gmail.com>
2022-07-27 17:00:47 +0300

    fix: container height

--- src/components/containers/Container.js
+++ src/components/containers/Container.js
@@ -3,7 +3,7 @@ import React from "react";
 const Container = ({ children, style = "" }) => {
   return (
     <ul
-      className={`flex flex-col justify-start items-start w-full h-0 mt-2 bg-gray-100 rounded-md shadow-md overflow-hidden ${style}`}
+      className={`flex flex-col justify-start items-start w-full mt-2 bg-gray-100 rounded-md shadow-md overflow-hidden ${style}`}
     >
       {children}
     </ul>
