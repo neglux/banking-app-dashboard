a63717cc0e9cbf9287d0edd5c0350ce0375e6374
neglux <akcaaytuna@gmail.com>
2022-07-27 16:21:16 +0300

    fix: layout

--- src/components/containers/Section.js
+++ src/components/containers/Section.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Section = ({ children, style = "" }) => {
-  return <section className={`mx-10 my-5 ${style}`}>{children}</section>;
+  return <section className={`mx-10 my-2 ${style}`}>{children}</section>;
 };
 
 export default Section;
