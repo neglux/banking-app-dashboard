24e782a3e1d31806146e68e94d8f02f93231b9b5
neglux <akcaaytuna@gmail.com>
2022-07-27 15:35:08 +0300

    develop: adding date

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,19 @@ const Navbar = () => {
   const { activeUser, logout } = useGlobalContext();
   return (
     <nav className="flex w-full items-center justify-between px-10 py-8 bg-gray-900 text-gray-100">
-      <span>
-        {activeUser ? `${welcomeMsg} ${activeUser.firstName}` : loginMsg}
-      </span>
+      <div className="flex flex-col">
+        <span>
+          {activeUser ? `${welcomeMsg} ${activeUser.firstName}` : loginMsg}
+        </span>
+        <span className="text-xs text-slate-200 font-semibold mt-1">
+          {Intl.DateTimeFormat(activeUser.language, {
+            day: "2-digit",
+            weekday: "short",
+            month: "long",
+            year: "numeric",
+          }).format(new Date())}
+        </span>
+      </div>
       <button
         className="px-12 py-1 rounded-sm bg-gray-700 hover:bg-gray-600"
         onClick={() => {
